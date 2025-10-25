495e64c57db564c5cd949495e37a3b4ad0a5cb9e
resoul <resoul@bk.ru>
2025-10-26 01:33:23 +0300

    let's make new hello world here

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
-import { Router } from './router/Router'
-import { routes } from './routes'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { DarkModeProvider } from "./darkMode";
+import { AvatarProvider } from "./avatar/AvatarProvider.tsx";
+import { ChatLayout } from "./ChatLayout.tsx";
+import { WebcamComponent } from "./video/WebcamComponent.tsx";
 
-export default function App() {
-    return <Router routes={routes} />
+function App() {
+    return (
+        <DarkModeProvider initialMode="light">
+            <AvatarProvider initialAvatar="/images/avatar/avatar-12.jpg" initialUsername="Travis Fuller">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/messenger" element={<ChatLayout />} />
+                        <Route path="/messenger/:contactId" element={<ChatLayout />} />
+                        <Route path="/video" element={<WebcamComponent />} />
+                        <Route path="/" element={<Navigate to="/messenger" replace />} />
+                    </Routes>
+                </BrowserRouter>
+            </AvatarProvider>
+        </DarkModeProvider>
+    );
 }
+
+export default App
